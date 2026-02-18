d6a6c0a6681bb7624aa273a8103a45695afae026
Citadoras <puferis0@gmail.com>
2026-02-18 16:47:46 +0200

    Added descriptions about works

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -15,21 +15,21 @@ export default function Works() {
 
                 <div className="d-flex justify-content-start">
                     <div className="projectBox border border-2 border-dark p-3 my-2">
-                        <h1>Project 1</h1>
-                        <p className='projectDescription pb-3 pt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+                        <h1>Single Page Application (SPA)</h1>
+                        <p className='projectDescription pb-3 pt-3'>Responsive single-page application (SPA) This project focuses on complex layouts, interactive elements, and clean HTML.</p>
                         <div className="d-flex justify-content-center gap-3">
-                            <a href=""><img className='refLogo m-1' src={githubLogo} alt="GitHub"/></a>
-                            <a href=""><img className='refLogo m-1' src={demo} alt="Demo"/></a>
+                            <a href="https://github.com/ppikelis99/Single-Page-Application"><img className='refLogo m-1' src={githubLogo} alt="GitHub"/></a>
+                            <a href="https://pauliusp.lt/spa/"><img className='refLogo m-1' src={demo} alt="Demo"/></a>
                         </div>
                     </div>
                 </div>
 
                 <div className="d-flex justify-content-start">
                     <div className="projectBox border border-2 border-dark p-3 my-2 m-md-5">
-                        <h1>Project 2</h1>
-                        <p className='projectDescription pb-3 pt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
-                        <a href=""><img className='refLogo m-1' src={githubLogo} alt="GitHub"/></a>
-                        <a href=""><img className='refLogo m-1' src={demo} alt="Demo"/></a>
+                        <h1>eShop Project</h1>
+                        <p className='projectDescription pb-3 pt-3'>A modern, multi-page e-commerce website designed for a seamless shopping experience. Users can browse products, manage their shopping cart, and view detailed product information in real-time.</p>
+                        <a href="https://github.com/ppikelis99/eShop"><img className='refLogo m-1' src={githubLogo} alt="GitHub"/></a>
+                        <a href="https://pauliusp.lt/eshop/"><img className='refLogo m-1' src={demo} alt="Demo"/></a>
                     </div>
                 </div>
 
