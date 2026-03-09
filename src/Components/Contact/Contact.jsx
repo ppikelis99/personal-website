e9e4f9b4878c36075e50b062890b2c096484fdaf
Citadoras <puferis0@gmail.com>
2026-03-09 12:50:14 +0200

    contact info changed

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,10 +4,10 @@ export default function Contact() {
         <div className="w-100 contactContainer text-center p-5">
             <h1>Contact</h1>
             <p>Feel free to reach out to me through email or social media!</p>
-            <div className="d-flex justify-content-center gap-4 mt-4">
-                <p>Ppikelis9@gmail.com</p>
-                <a href="https://www.linkedin.com/in/paulius-pikelis-755a16381/">LinkedIn</a>
-                <a href="">GitHub</a>
+            <div className="d-flex flex-column justify-content-center gap-4 mt-4">
+                <p>Email: <a href="mailto:ppikelis9@gmail.com">Ppikelis9@gmail.com</a></p>
+                <a className='urlLinks fs-5' href="https://www.linkedin.com/in/paulius-pikelis-755a16381/">LinkedIn</a>
+                <a className='urlLinks fs-5' href="https://github.com/ppikelis99?tab=repositories">GitHub</a>
             </div>
         </div>
     );
