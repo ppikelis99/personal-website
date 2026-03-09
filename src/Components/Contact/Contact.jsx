f6329e3079c1b3aed8f01524d61c8449b09b8b2b
Citadoras <puferis0@gmail.com>
2026-03-09 12:03:44 +0200

    dark/light theme on contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,11 +1,11 @@
-
+import './Contact.css'
 export default function Contact() {
     return (
         <div className="w-100 contactContainer text-center p-5">
             <h1>Contact</h1>
             <p>Feel free to reach out to me through email or social media!</p>
             <div className="d-flex justify-content-center gap-4 mt-4">
-                <a href="">Email</a>
+                <p>Ppikelis9@gmail.com</p>
                 <a href="https://www.linkedin.com/in/paulius-pikelis-755a16381/">LinkedIn</a>
                 <a href="">GitHub</a>
             </div>
